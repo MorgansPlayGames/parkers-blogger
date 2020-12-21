cc96fcc36be0d6e5b26ff80c5c0dcebc98b8d59a
Parker Morgan <jpmaster13@msn.com>
2020-12-21 14:20:41 -0700

    Login screen no formantting

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -16,7 +16,7 @@ router.get('/', async (req, res) => {
       });
   
       // Serialize data so the template can read it
-      const posts = projectData.map((project) => project.get({ plain: true }));
+      const posts = postData.map((post) => post.get({ plain: true }));
 
       res.render('homepage', {
         posts,
@@ -26,5 +26,15 @@ router.get('/', async (req, res) => {
         res.status(500).json(err)
     }
 });
+
+router.get('/login', (req, res) => {
+    // If the user is already logged in, redirect the request to another route
+    if (req.session.logged_in) {
+      res.redirect('/profile');
+      return;
+    }
+  
+    res.render('login');
+  });
 
 module.exports = router;
